e083b6f35808832960cb7ae2a69a888da881b960
Munana Clinton <munanaclinton@gmail.com>
2023-05-03 09:19:03 +0200

    Add src directory

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+import './style.css';
+
+const listOfActivity = document.querySelector('ul');
+const activities = [
+  {
+    index: 1,
+    description: 'go shopping',
+    state: true,
+  },
+  {
+    index: 2,
+    description: 'go to the gym',
+    state: false,
+  },
+  {
+    index: 3,
+    description: 'go to school',
+    state: true,
+  },
+];
+
+const display = () => {
+  activities.forEach((activity) => {
+    listOfActivity.innerHTML
+  += `<li id="${activity.index}"><input type="checkbox" required>
+  <span>${activity.description}</span></li>`;
+  });
+};
+
+display();
