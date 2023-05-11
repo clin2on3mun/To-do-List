5e4b9232ec3ee54773e0d3128238b80432a36ac8
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-05-11 13:53:55 +0530

    add testcases for add and remove

--- tests/addAndRemove.test.js
+++ tests/addAndRemove.test.js
@@ -1,2 +1,19 @@
 const { addTask, removeTask } = require('../_mocks_/addAndRemove.js');
 
+describe('Add and remove', () => {
+  test('add item', () => {
+    expect(addTask({ tasks: 'go to church', completed: false, index: 1 })).not.toBe([]);
+  });
+  test('add item', () => {
+    expect(addTask({ tasks: 'go to school', completed: true, index: 2 })).not.toBe([]);
+  });
+  test('add item', () => {
+    expect(addTask({ tasks: 'go to school', completed: true, index: 3 })).toHaveLength(3);
+  });
+  test('remove Task', () => {
+    expect(removeTask(0)).toBeTruthy();
+  });
+  test('remove Task', () => {
+    expect(removeTask(0)).toHaveLength(1);
+  });
+});
