2d461bdabf7a82f073238a1ede925761b11e33e9
Munana Clinton <munanaclinton@gmail.com>
2023-05-11 16:54:09 +0200

    Add changes to tests directory and _mocks_ directory

--- _mocks_/testing.js
+++ _mocks_/testing.js
@@ -1,4 +1,4 @@
-const activity = [{task:"go to mall",index:1,completed:false},{task:"go to gym",index:2,completed:false},{task:"eat lunch",index:3,completed:false}];
+const activity = [{ task: 'go to mall', index: 1, completed: false }, { task: 'go to gym', index: 2, completed: false }, { task: 'eat lunch', index: 3, completed: false }];
 
 const editTask = (task) => {
   const editSpace = document.createElement('input');
@@ -8,28 +8,18 @@ const editTask = (task) => {
   task.innerHTML = '';
   task.parentNode.insertBefore(editSpace, task.nextSibling);
   return editSpace;
-}
+};
 
 const complete = (index) => {
-  // let status=false;
-  // activity.forEach((item) => {
-  //   status = item.completed;
-  //   if (item.index === index) {
-  //     if (!status) {
-  //       status = true;
-  //     }
-  //   }
-  // });
-
-  const status = activity.find((item)=>item.index===index);
+  const status = activity.find((item) => item.index === index);
   status.completed = true;
   return status;
 };
 
-const clearAll=()=>{
+const clearAll = () => {
   const activities = activity.filter((item) => item.completed === false);
   return activities;
-}
+};
 
 exports.editTask = editTask;
 exports.complete = complete;
--- tests/testing.test.js
+++ tests/testing.test.js
@@ -1,7 +1,6 @@
-const { editTask, complete, clearAll} = require('../_mocks_/testing.js');
+const { editTask, complete, clearAll } = require('../_mocks_/testing.js');
 
 describe('edit', () => {
-  
   test('edit task description', () => {
     const expand = document.createElement('span');
     expand.textContent = 'hello';
@@ -9,16 +8,15 @@ describe('edit', () => {
     expect(editTask(expand)).toBeTruthy();
   });
 
-  test('for update',() =>{
-    expect(complete(1)).toStrictEqual({task:"go to mall",index:1,completed:true});
-  })
+  test('for update', () => {
+    expect(complete(1)).toStrictEqual({ task: 'go to mall', index: 1, completed: true });
+  });
 
-  test('for update',() =>{
-    expect(complete(3)).toStrictEqual({task:"eat lunch",index:3,completed:true});
-  })
+  test('for update', () => {
+    expect(complete(3)).toStrictEqual({ task: 'eat lunch', index: 3, completed: true });
+  });
 
-  test('clear all completed',() =>{
-    expect(clearAll()).toStrictEqual([{task:"go to gym",index:2,completed:false}]);
-  })
-  
+  test('clear all completed', () => {
+    expect(clearAll()).toStrictEqual([{ task: 'go to gym', index: 2, completed: false }]);
+  });
 });
